65bbcbe41e28bd64e05792d7744cee239c050a00
Mage <56733007+Meoooww@users.noreply.github.com>
2022-10-25 21:44:56 +0800

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ from utils import *
 from data import *
 from torchvision.utils import save_image
 from PIL import Image
-net=UNet(3).cuda()
+net=UNet(3).cuda() # 3 类别数
 
 weights='params/unet.pth'
 if os.path.exists(weights):
@@ -21,16 +21,21 @@ else:
 _input=input('please input JPEGImages path:')
 
 img=keep_image_size_open_rgb(_input)
-img_data=transform(img).cuda()
-img_data=torch.unsqueeze(img_data,dim=0)
-net.eval()
+# print(img.shape)
+img_data=transform(img).cuda() #转换成张量
+# print(img_data.shape)
+img_data=torch.unsqueeze(img_data,dim=0) #升维
+# print(img_data.shape)
+
+# net.eval()
 out=net(img_data)
-out=torch.argmax(out,dim=1)
-out=torch.squeeze(out,dim=0)
-out=out.unsqueeze(dim=0)
-print(set((out).reshape(-1).tolist()))
-out=(out).permute((1,2,0)).cpu().detach().numpy()
-cv2.imwrite('result/result.png',out)
-cv2.imshow('out',out*255.0)
-cv2.waitKey(0)
+save_image(out,'result/result.jpg')
+# out=torch.argmax(out,dim=1)
+# out=torch.squeeze(out,dim=0)
+# out=out.unsqueeze(dim=0)
+# print(set((out).reshape(-1).tolist()))
+# out=(out).permute((1,2,0)).cpu().detach().numpy()
+# cv2.imwrite('result/result.png',out)
+# cv2.imshow('out',out*255.0)
+# cv2.waitKey(0)
 
